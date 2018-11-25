cac0315a3764d2258a1c4d623a1bda63072556e7
riverplant <riverplant@hotmail.com>
2018-11-25 13:31:56 -0500

    DemoApplication

---
+++ river-demo/src/main/java/com/river/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.river;
+/**
+ * 
+ * @author riverplant
+ *
+ */
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
